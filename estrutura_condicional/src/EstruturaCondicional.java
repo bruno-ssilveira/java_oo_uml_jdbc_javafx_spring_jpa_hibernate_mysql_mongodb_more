4c9343f9123121e76787bb4fc86278a65ffa1013
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2024-01-22 22:00:24 -0300

    att-exercicio

--- estrutura_condicional/src/EstruturaCondicional.java
+++ estrutura_condicional/src/EstruturaCondicional.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class EstruturaCondicional {
 
     /*
@@ -12,5 +15,46 @@ public class EstruturaCondicional {
         Nunca colocar condição na frente do else, somente do if...
 
     */
+
+    public static void main(String[] args) {
+
+        /* Exercício 1: Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não. */
+        Scanner scan = new Scanner(System.in);
+        String result = "";
+        int num = 0;
+
+        try {
+            System.out.println("Digite um número inteiro: ");
+            num = scan.nextInt();
+
+            if (num < 0) {
+                result = "negative";
+            } else {
+                result = "positive";
+            }
+
+            System.out.println("The number is " + num + " and is " + result);
+
+        } catch (InputMismatchException e) {
+            System.err.println("Error: the number need to be integer.");
+        }
+
+        /* Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. */
+        try {
+            System.out.println("Digite um número inteiro: ");
+            num = scan.nextInt();
+
+            if (num % 2 == 0) {
+                result = "par";
+            } else {
+                result = "impar";
+            }
+
+            System.out.println("The number is " + num + " and is " + result);
+
+        } catch (InputMismatchException e) {
+            System.err.println("Error: the number need to be integer.");
+        }
+    }
 
 }
